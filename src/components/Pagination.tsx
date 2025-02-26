22763285c5d3545b64cfc979a0608a6bd6202e61
unknown <voweiroajenaghonre@gmail.com>
2025-02-26 15:35:07 +0100

    done

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -8,15 +8,15 @@ const Pagination: React.FC<PaginationProps> = ({ currentPage, totalPages, onPage
   return (
     <div className="mt-6 flex justify-center space-x-4">
       <button
-        className={`px-4 py-2 border ${currentPage === 1 ? "opacity-50 cursor-not-allowed" : ""}`}
+        className={`px-4  bg-blue-600  rounded-lg hover:bg-green-400 py-2 border ${currentPage === 1 ? "opacity-50 cursor-not-allowed" : ""}`}
         onClick={() => onPageChange(currentPage - 1)}
         disabled={currentPage === 1}
       >
         Previous
       </button>
-      <span className="px-4 py-2">Page {currentPage} of {totalPages || 1}</span>
+      <span className="px-4  py-2">Page {currentPage} of {totalPages || 1}</span>
       <button
-        className={`px-4 py-2 border ${currentPage >= totalPages ? "opacity-50 cursor-not-allowed" : ""}`}
+        className={`px-4  bg-blue-600  rounded-lg  hover:bg-green-400 py-2 border ${currentPage >= totalPages ? "opacity-50 cursor-not-allowed" : ""}`}
         onClick={() => onPageChange(currentPage + 1)}
         disabled={currentPage >= totalPages}
       >
